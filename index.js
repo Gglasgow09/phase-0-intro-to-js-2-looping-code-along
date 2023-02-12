c7957d97fbcc4cd85a5a29c6629cd4086e9d7020
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-02-12 13:49:31 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+function writeCards(names, event) {
+  let individualCards = []
+  for (let i = 0; i < names.length; i++) {
+    individualCards.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+   
+  }
+
+  return individualCards;
+}
+
+function countDown() {
+  let numberCountdown = 11
+  while(numberCountdown --) {
+    console.log(numberCountdown)
+  }
+}
+
